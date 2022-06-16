2688db0e2c278b43c536536ecf45a30e95f414f8
mayorD19 <dominionoje@gmail.com>
2022-06-16 02:06:02 +0100

    A Django model setup

---
+++ I4G012633GML/blog/models.py
@@ -0,0 +1,16 @@
+from calendar import month
+from unittest.util import _MAX_LENGTH
+from django.contrib.auth.models import User
+from django.db import models
+# from datetime import datetime,date
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
